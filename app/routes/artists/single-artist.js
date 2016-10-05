40a9a3783e71182e0926065e085c7a8d89833996
Sarah Wu <sarahlwu@usc.edu>
2016-10-04 23:54:51 -0700

    working except for individual song attributes

--- app/routes/artists/single-artist.js
+++ app/routes/artists/single-artist.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     //function where we fetch data
-    var url = `http://itp-api.herokuapp.com/api/artists/${params.id}/songs&callback=?`;
+    var url = `http://itp-api.herokuapp.com/api/artists/${params.id}/songs/`;
     return $.getJSON(url);
+  },
+  afterModel: function(model) {
+    console.log(model);
   }
 });
